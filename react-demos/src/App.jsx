070cfb91c115e75fec258aa560a0c4604849933a
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-04-15 23:41:53 +0530

    day2 learning reactjs

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,13 +1,14 @@
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-
 const App = () => {
+  const myName = "YashChhatrala"
+  const multiply = (a, b) => a * b;
+  const specialClass = 'special-class'
   return (
   <>
-    <Header />
-    <MainContent />
-    <Footer />
+    <p>2 + 2 = {2+2}</p>
+    <h2>{myName}</h2>
+    <p>My Friends List: {["Yash", "Rahul", "Jay", "Sagar"]}</p>
+    <p>2 * 10 = {multiply(2,10)}</p>
+    <p className={specialClass}>This is special class</p>
   </>
   )
 }
